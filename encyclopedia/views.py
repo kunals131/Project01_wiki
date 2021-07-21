df7c1b9583d771fc6f4878abeb898c075abe92c8
unknown <kunalsangtiani03@gmail.com>
2021-07-21 15:06:23 +0530

    Added search, add, edit, random

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,21 @@
+from django.forms.forms import Form
+from django.forms.formsets import INITIAL_FORM_COUNT
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from . import util
+import secrets  
 
 from markdown2 import Markdown
+
+from django import forms
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Entry title", widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    content = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -40,4 +51,52 @@ def search(request):
             "value"  : value
         })
 
+def newEntry(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            if (util.get_entry(title) is None or form.cleaned_data["edit"] is True):
+                util.save_entry(title,content)
+                return HttpResponseRedirect(reverse("entry", kwargs={'entry' : title}))
+            else: 
+                return render(request,"encyclopedia/newEntry.html",{
+                    "form" : form,
+                    "existing": True,
+                    "entry" : title
+                })
+        else:
+            return render(request,"encyclopedia/newEntry.html", {
+                "form" : form,
+                "existing" : False
+            })
+    else:
+        return render(request,"encyclopedia/newEntry.html",{
+            "form" : NewEntryForm(),
+            "existing" : False
+        })
+
+def edit(request, entry):
+    entryPage = util.get_entry(entry)
+    if entryPage is None:
+        return render(request, "encyclopedia/Error404.html", {
+            "entryTitle" : entry
+        })
+    else:
+        form = NewEntryForm()
+        form.fields["title"].initial = entry
+        form.fields["content"].initial = entryPage
+        form.fields["title"].widget = forms.HiddenInput()
+        form.fields["edit"].initial = True
+        return render(request,"encyclopedia/newEntry.html", {
+            "form" : form,
+            "edit" : form.fields["edit"].initial,
+            "entryTitle": form.fields["title"].initial 
+        })
+
+def random(request):
+    listofitem = util.list_entries()
+    randomentry = secrets.choice(listofitem)
+    return HttpResponseRedirect(reverse("entry",kwargs={'entry': randomentry}))
 
